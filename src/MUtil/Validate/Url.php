c525f5217805007961c11c78fa2459921b70cc23
mddejong <mjong@magnafacta.nl>
2020-09-29 16:40:34 +0200

    Fixed #594 Block URL validation in interface

--- src/MUtil/Validate/Url.php
+++ src/MUtil/Validate/Url.php
@@ -1,38 +1,12 @@
 <?php
 
 /**
- * Copyright (c) 2011, Erasmus MC
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *    * Redistributions of source code must retain the above copyright
- *      notice, this list of conditions and the following disclaimer.
- *    * Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer in the
- *      documentation and/or other materials provided with the distribution.
- *    * Neither the name of Erasmus MC nor the
- *      names of its contributors may be used to endorse or promote products
- *      derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
  *
  * @package    MUtil
  * @subpackage Validate
  * @author     Matijs de Jong <mjong@magnafacta.nl>
  * @copyright  Copyright (c) 2011 Erasmus MC
  * @license    New BSD License
- * @version    $Id$
  */
 
 /**
@@ -51,6 +25,11 @@ class MUtil_Validate_Url extends \Zend_Validate_Abstract
     const ERROR_SITE_NOT_FOUND = 'siteNotFound';
     const ERROR_URL_NOT_VALID = 'urlNotFound';
 
+    /**
+     * @var bool Disable checking of url's for installations behind firewalls
+     */
+    public static $disabled = false;
+    
     /**
      * @var array Message templates
      */
@@ -82,6 +61,11 @@ class MUtil_Validate_Url extends \Zend_Validate_Abstract
      */
     public function isValid($value, $context = array())
     {
+        if (self::$disabled) {
+            // Do not perform check when disabled
+            return true;
+        }
+        
         $this->_setValue($value);
 
         if ($value) {
